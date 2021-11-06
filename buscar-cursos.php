f8df59ffce117dd06d6d0b9ed67a3012fd4d9eed
Vagner Alves Leite <vleite@viceri.com.br>
2021-11-06 18:03:10 -0300

    Ajustando echo exibeMensagem

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -12,5 +12,5 @@ $buscador = new Buscador($client, $crawler);
 $cursos = $buscador->buscar('/cursos-online-programacao/php');
 
 foreach ($cursos as $curso) {
-    echo exibeMensagem($curso);
+    exibeMensagem($curso);
 }
